728cd726afa0235de6729ac328aa899caf16f4f3
Alice <alicewulfric@gmail.com>
2023-04-30 21:05:09 -0300

    You can turn ON the LED nowgit add .!

--- components/Screens/devices_screens/led.tsx
+++ components/Screens/devices_screens/led.tsx
@@ -1,16 +1,17 @@
 import { View, Text, Pressable} from "react-native";
-import BluetoothSerial from "react-native-bluetooth-serial-next";
+import BluetoothSerial from "react-native-bluetooth-serial";
+import { Buffer } from "buffer";
+var iconv = require('iconv-lite');
 
 import { styles } from '../../styles';
+import { house_write } from '../../blue';
 
-export const LedScreen = ({navigation}) => {
+export const LedScreen = ({navigation}) => { 
+    const device = 'LED';
+    const data = 'TURN_ON';
     return(
         <View style={styles.container}>
-            <Pressable onPress={async() => {
-                                    BluetoothSerial.write("A")
-                                    .then(()=>{console.log("sent A")})
-                                    .catch(()=>{console.log("Error");})
-                                }}>
+            <Pressable onPress={()=>house_write(device, data)}>
                 <View style={styles.deviceButton}></View>
             </Pressable>
         </View>
